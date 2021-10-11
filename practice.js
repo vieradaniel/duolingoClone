62d32548b247711a8576bc741b6d30eb2eb498f7
vieradaniel <dariennx@hotmail.com>
2021-10-11 19:17:57 -0300

    working with lessons

--- practice.js
+++ practice.js
@@ -1,1 +1,7 @@
 
+const lesson = document.querySelector('.lesson-icon-border');
+const lessonMenu= document.querySelector('.lesson-menu');
+
+lesson.addEventListener("click",function(){
+    lessonMenu.classList.toggle('show');
+})
